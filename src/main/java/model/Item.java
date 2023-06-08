099028db764b2a1d1c8fff63e6cff302bae2e9c0
Limo <christmasboom@mail.ru>
2023-06-08 15:17:50 +0300

    Начало ДЗ

---
+++ src/main/java/model/Item.java
@@ -0,0 +1,22 @@
+package model;
+
+public class Item {
+    private Long id;
+
+    public Item() {
+    }
+
+    public Item(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
+
+
